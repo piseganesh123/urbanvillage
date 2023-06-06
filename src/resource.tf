07e6848a76bbdf6a9e6b71df73eb6dded1d416f2
piseganesh123 <piseganesh123@gmail.com>
2023-06-06 11:45:15 +0000

    updated resource.tf

---
+++ src/resource.tf
@@ -0,0 +1,11 @@
+resource "aws_subnet" "demo-subnet" {
+  vpc_id = data.aws_vpc.demo-vpc-data.id
+  cidr_block = "10.0.0.0/28"
+  tags = {
+    "Name" = "tf-demo-subnet"
+    "purpose" = "demo"
+  }    
+}
+data "aws_vpc" "demo-vpc-data" {
+  cidr_block = "10.0.0.0/24"
+}
