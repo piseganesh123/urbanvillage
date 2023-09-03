972aaebeb5e66a39f584b2de78291d8b8a23faa4
piseganesh123 <piseganesh123@gmail.com>
2023-09-03 06:45:13 +0000

    updated  code

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -2,6 +2,6 @@
 
 sudo docker stop demo-farma-app && sudo docker rm demo-farma-app
 
-sudo docker build -t demo-urban-farm:1.0.1 ../Dockerfile
+sudo docker build -t demo-urban-farm:1.0.1 .
 
 sudo docker run -d -p 80:80 --name demo-farma-app demo-urban-farm:1.0.1
